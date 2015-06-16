79fbd314a146f9ed2dd5692f83d8d4e356d48506
Klaus Laube <klaus@corp.globo.com>
2015-06-16 11:21:17 -0300

    Enabling autocorrection

--- .my-aliases.sh
+++ .my-aliases.sh
@@ -4,6 +4,7 @@ alias netstat_osx='sudo lsof -i -P'
 alias tm='top -o vsize' # memory
 alias tu='top -o cpu' # cpu
 alias tree='ls -R | grep ":$" | sed -e "s/:$//" -e "s/[^-][^\/]*\//--/g" -e "s/^/   /" -e "s/-/|/"'
+alias zshconfig="vim ~/.zshrc"
 
 # Python tools
 alias pyclean='find . -name "*.pyc" -exec rm {} \;'
