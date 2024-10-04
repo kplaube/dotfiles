5fc1d76cdafc4a35e997d6f94bed1d3a02a74099
Klaus Laube <kplaube@gmail.com>
2024-10-04 09:19:23 +0200

    Fix tmux error message

--- my-aliases.sh
+++ my-aliases.sh
@@ -5,6 +5,8 @@ alias workon_dotfiles="cd $HOME/Workspace/dotfiles/"
 # Git workflow helpers
 
 alias gpm="git checkout main; git pull origin main --rebase -f"
+alias gpma="git checkout master; git pull origin master --rebase -f"
 alias gpd="git checkout develop; git pull origin develop --rebase -f"
 alias gprm="gpm; git checkout -; git rebase main"
+alias gprma="gpma; git checkout -; git rebase master"
 alias gprd="gpd; git checkout -; git rebase develop"
