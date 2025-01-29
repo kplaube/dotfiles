515b0aed48c064969698d44abd6a37b37fd2dda0
Klaus Laube <kplaube@gmail.com>
2025-01-29 17:41:53 +0100

    foo

---
+++ setup-linux.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# tmux
+# ----
+echo ">> Installing tmux..."
+
+sudo apt install tmux
+
+if [ ! -f $HOME/.tmux/plugins/tpm ]; then
+  echo ">> Installing tmux plugin manager..."
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+if [ -f $HOME/.tmux.conf ]; then
+  echo ">> Overwriting tmux configuration..."
+  mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
+fi
+
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
+
+echo ">> Done"
+echo ""
+
+# fish
+# ----
+echo ">> Installing fish shell..."
+sudo apt install fish
+
+echo ">> Done"
+echo ""
+
+# Alacritty
+# ---------
+echo ">> Installing Alacritty..."
+sudo apt install alacritty
+
+echo ">> Configuring Alacritty..."
+mv $HOME/.config/alacritty $HOME/.config/alacritty.bak
+ln -s $PWD/alacritty/ $HOME/.config/alacritty
+
+echo ">> Done"
+echo ""
+
+# Zed
+# ---
+ZED_SETTINGS_FOLDER=$HOME/.config/zed
+
+echo ">> Installing Zed and custom settings..."
+brew install --cask zed
+
+mv -f $ZED_SETTINGS_FOLDER/settings.json /tmp/
+mv -f $ZED_SETTINGS_FOLDER/tasks.json /tmp/
+
+ln -s $PWD/zed/settings.json $ZED_SETTINGS_FOLDER/settings.json
+ln -s $PWD/zed/tasks.json $ZED_SETTINGS_FOLDER/tasks.json
+
+echo ">> Done"
+echo ""
--- zprofile
+++
@@ -1,7 +0,0 @@
-# Set PATH, MANPATH, etc., for Homebrew.
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-# Pyenv stuff
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
