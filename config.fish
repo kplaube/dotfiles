43b4e8bf41d5e207e900546d46f7921b6a17dbdc
Klaus Laube <kplaube@gmail.com>
2025-07-28 11:09:41 +0200

    Small tweaks and support to Rancher

--- config.fish
+++ config.fish
@@ -17,6 +17,9 @@ test -d $PYENV_ROOT/bin; and fish_add_path $PYENV_ROOT/bin
 string match -q "$TERM_PROGRAM" vscode
 and . (code --locate-shell-integration-path fish)
 
+# Rancher Desktop
+set --export --prepend PATH "/Users/klaube/.rd/bin"
+
 if status is-interactive
     pyenv init - fish | source
     nvm use lts >/dev/null 2>&1
